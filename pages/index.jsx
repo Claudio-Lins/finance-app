6c38591a99e6d0d674d5898f872e31480c9e12b5
Claudio Lins <69011104+Claudio-Lins@users.noreply.github.com>
2021-07-25 19:08:55 +0100

    datas implementadas

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,15 @@
 import Head from "next/head";
+import React from "react";
+import moment from "moment";
 import { useState, useEffect } from "react";
 
-import api from "../uteis/configApi"
+import api from "../uteis/configApi";
 
-export default function Home() {
+export const Home = () => {
   const [data, setData] = useState([]);
+  const [saldo, setSaldo] = useState("");
+  const [valorPagamento, setValorPagamento] = useState("");
+  const [valorRecebido, setValorRecebido] = useState("");
 
   let dataAtual = new Date();
   let ano = dataAtual.getFullYear();
@@ -15,73 +20,78 @@ export default function Home() {
     mes,
   });
 
+  const [status, setStatus] = useState({
+    type: "",
+    mensagem: "",
+  });
+
   const anterior = async () => {
     if (dateView.mes === 1) {
+      ano = dateView.ano - 1;
+      mes = 12;
       setDateView({
-        ano: dateView.ano - 1,
-        mes: 12,
+        ano,
+        mes,
       });
+      listarExtrato(mes, ano);
     } else {
+      ano = dateView.ano;
+      mes = dateView.mes - 1;
       setDateView({
-        ano: dateView.ano,
-        mes: dateView.mes - 1,
+        ano,
+        mes,
       });
+      listarExtrato(mes, ano);
     }
   };
   const proximo = async () => {
     if (dateView.mes === 12) {
+      ano = dateView.ano + 1;
+      mes = 1;
       setDateView({
-        ano: dateView.ano + 1,
-        mes: 1,
+        ano,
+        mes,
       });
+      listarExtrato(mes, ano);
     } else {
+      ano = dateView.ano;
+      mes = dateView.mes + 1;
       setDateView({
-        ano: dateView.ano,
-        mes: dateView.mes + 1,
+        ano,
+        mes,
       });
+      listarExtrato(mes, ano);
     }
   };
 
-  const listarExtrato = async (e) => {
+  const listarExtrato = async (mes, ano) => {
+    if (mes === undefined && ano === undefined) {
+      let dataAtual = new Date();
+      ano = dataAtual.getFullYear();
+      mes = dataAtual.getMonth() + 1;
+    }
 
-    await api.get("/listar/01/08/2020")
-    .then((response) => {
-      // console.log(response);
-    }).catch((err) => {
-      // console.log(err.response);
-    });
-
-    let valores = [
-      {
-        id: 1,
-        nome: "Uber",
-        valor: 23.45,
-        tipo: 1,
-        situacao: "x"
-      },
-      {
-        id: 2,
-        nome: "Bolt",
-        valor: 3.45,
-        tipo: 1,
-        situacao: "x"
-      },
-      {
-        id: 3,
-        nome: "Combustível",
-        valor: 70.89,
-        tipo: 2,
-        situacao: "y"
-      },
-      {
-        id: 4,
-        nome: "FreeNow",
-        valor: 10.19,
-        tipo: 1,
-        situacao: "x"
-      },
-    ];
-    setData(valores);
+    await api
+      .get("http://localhost:8080/listar/" + mes + "/" + ano)
+      .then((response) => {
+        setData(response.data.lancamentos);
+        setSaldo(response.data.saldo);
+        setValorPagamento(response.data.valorPagamento);
+        setValorRecebido(response.data.valorRecebido);
+      })
+      .catch((err) => {
+        if (err.response) {
+          setStatus({
+            type: "erro",
+            mensagem: err.response.data.mensagem,
+          });
+        } else {
+          setStatus({
+            type: "erro",
+            mensagem: "Tente mais tarde!",
+          });
+        }
+      });
   };
 
   useEffect(() => {
@@ -91,14 +101,15 @@ export default function Home() {
   return (
     <div className="flex flex-col justify-center">
       <Head>
-        <title>Create Next App</title>
+        <title>Controle Financeiro - CELKE</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <header className="bg-purple-600 w-full h-[100px] flex justify-center items-center">
+      <header className="bg-purple-600 w-full h-[100px] flex flex-col justify-center items-center">
         <h1 className=" font-bold text-4xl text-white tracking-wide">
           Controle Financeiro
         </h1>
+        {status.type === "erro" ? <span>{status.mensagem}</span> : ""}
       </header>
 
       <main className="flex flex-col p-4 items-center justify-center w-full flex-1 text-center">
@@ -123,33 +134,93 @@ export default function Home() {
           </button>
         </div>
         <div className="bg-gray-100 w-full mt-4 rounded-md shadow-md">
-          <div className="grid grid-cols-5 py-2 rounded-t-md bg-black text-white font-semibold w-full">
+          <div className="grid grid-cols-6 py-2 rounded-t-md bg-black text-white font-semibold w-full">
             <span>ID</span>
             <span>Nome</span>
             <span>Tipo</span>
             <span>Situação</span>
+            <span>Data</span>
             <span>Valor</span>
           </div>
           <div className="">
             {data.map((item) => (
-              <div key={item.id} className="grid grid-cols-5 py-2">
+              <div key={item.id} className="grid grid-cols-6 py-2">
                 <p className="">{item.id}</p>
                 <p className="">{item.nome}</p>
                 <p className="">
                   {item.tipo === 1 ? (
-                    <p className="text-green-600 font-black">+</p>
+                    <p className="text-red-600 font-light">Despesa</p>
                   ) : (
-                    <p className="text-red-600 font-black">-</p>
+                    ""
+                  )}
+                  {item.tipo === 2 ? (
+                    <p className="text-green-600 font-light">Recebimento</p>
+                  ) : (
+                    ""
                   )}
                 </p>
-                <p className="">{item.situacao}</p>
-                <p className="">{item.valor}</p>
+                <p className="">
+                  {item.situacao === 1 ? (
+                    <p className="text-blue-600 font-light">Pago</p>
+                  ) : (
+                    ""
+                  )}
+                  {item.situacao === 2 ? (
+                    <p className="text-yellow-600 font-light">Pendente</p>
+                  ) : (
+                    ""
+                  )}
+                  {item.situacao === 3 ? (
+                    <p className="text-red-600 font-light">Atrasado</p>
+                  ) : (
+                    ""
+                  )}
+                </p>
+                <p className="">
+                  {moment(item.dataPagamento).format("DD/MMM/YYYY")}
+                </p>
+                <p className="">
+                  {new Intl.NumberFormat("pt-PT", {
+                    style: "currency",
+                    currency: "EUR",
+                  }).format(item.valor)}
+                </p>
+                {/* <p className="">{item.valor}</p> */}
               </div>
             ))}
-            <div></div>
+          </div>
+          <div className="flex justify-between bg-gray-500 px-36 py-4 rounded-b-lg">
+            <div>
+              <strong>Pagos</strong>
+            </div>
+            <div>
+              {new Intl.NumberFormat("pt-PT", {
+                style: "currency",
+                currency: "EUR",
+              }).format(valorPagamento)}
+            </div>
+            <div>
+              <strong>Recebidos</strong>
+            </div>
+            <div>
+              {new Intl.NumberFormat("pt-PT", {
+                style: "currency",
+                currency: "EUR",
+              }).format(valorRecebido)}
+            </div>
+            <div>
+              <strong>Saldo</strong>
+            </div>
+            <div>
+              {new Intl.NumberFormat("pt-PT", {
+                style: "currency",
+                currency: "EUR",
+              }).format(saldo)}
+            </div>
           </div>
         </div>
       </main>
     </div>
   );
-}
+};
+export default Home;
--- uteis/configApi.js
+++ uteis/configApi.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-    baseUrl: 'http://192.168.1.92:8080'
+    baseUrl: 'http://localhost:8080'
 });
