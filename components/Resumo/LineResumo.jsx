e28d7ea1cb904cad2b6e244d25c1fc24ff09e9c1
Claudio Lins <69011104+Claudio-Lins@users.noreply.github.com>
2021-07-27 08:01:51 +0100

    Update LineResumo.jsx

--- components/Resumo/LineResumo.jsx
+++ components/Resumo/LineResumo.jsx
@@ -4,12 +4,12 @@
 export default function LineResumo(props) {
     return (
         <div className='flex w-full px-4 justify-between'>
-            <div className='py-1'>{props.id}</div>
-            <div className='py-1 bg-gray-400'>{props.nome}</div>
-            <div className='py-1'>{props.tipo}</div>
-            <div className='py-1'>{props.situacao}</div>
-            <div className='py-1'>{props.date}</div>
-            <div className='py-1 font-semibold text-right'>{props.valor}</div>
+            <div className='py-1 w-full '>{props.id}</div>
+            <div className='py-1 w-full text-left'>{props.nome}</div>
+            <div className='py-1 w-full text-left '>{props.tipo}</div>
+            <div className='py-1 w-full '>{props.situacao}</div>
+            <div className='py-1 w-full '>{props.date}</div>
+            <div className='py-1 w-full  font-semibold text-right'>{props.valor}</div>
         </div>
     )
 }
